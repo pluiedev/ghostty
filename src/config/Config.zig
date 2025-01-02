7d2b53a59206d2addc2d81ae6a9ffce4f97b2929
Leah Amelia Chen <hi@pluie.me>
2025-01-02 23:53:22 +0800

    gtk: unify Wayland and X11 platforms

--- src/config/Config.zig
+++ src/config/Config.zig
@@ -5731,6 +5731,14 @@ pub const BackgroundBlur = union(enum) {
             ) catch return error.InvalidValue };
     }
 
+    pub fn enabled(self: BackgroundBlur) bool {
+        return switch (self) {
+            .false => false,
+            .true => true,
+            .radius => |v| v > 0,
+        };
+    }
+
     pub fn cval(self: BackgroundBlur) u8 {
         return switch (self) {
             .false => 0,
