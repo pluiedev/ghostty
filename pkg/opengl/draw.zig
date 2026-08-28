b87af8646f71de1398e665c361fe659b0707a72e
Leah Amelia Chen <hi@pluie.me>
2026-08-28 16:01:37 +0800

    pkg/opengl: replace cImport with translate-c
    General Zig 0.16+ futureproofing

--- pkg/opengl/draw.zig
+++ pkg/opengl/draw.zig
@@ -1,4 +1,4 @@
-const c = @import("c.zig").c;
+const c = @import("c");
 const errors = @import("errors.zig");
 const glad = @import("glad.zig");
 const Primitive = @import("primitives.zig").Primitive;
